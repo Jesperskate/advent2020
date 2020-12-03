7184b534d07de3a712a9b69679f7868cf122d71c
Jesper Voorendt <Jesper.voorendt@knab.nl>
2020-12-03 21:57:23 +0100

    Day 3 part two

--- Day 3/three.js
+++ Day 3/three.js
@@ -4,29 +4,55 @@ var loc = {
 }
 var trees = 0;
 var lastItem;
+let slopes = [];
+slopes.push(step(1, 1));
+resetLoc()
+slopes.push(step(3, 1));
+resetLoc()
+slopes.push(step(5, 1));
+resetLoc()
+slopes.push(step(7, 1));
+resetLoc()
+slopes.push(step(1, 2));
+console.log("Trees: " + trees);
 
-while (loc.y < data.length-1) {
-    
-    step(3, 1)
-}
-console.log("Trees found: "+ trees);
+console.table(slopes);
+console.table(slopes.reduce(function(a,b){return a*b;}));
 
 
-function step(xDir, yDir){
-    loc.x = loc.x + xDir;
-    loc.y = loc.y + yDir;
 
-    // Get Y line
-    let yLine = data[loc.y]
 
-    // Get X from yLine
-    let xOnY = yLine.charAt(loc.x);
-    console.log("There is a "+xOnY+" at x"+loc.x +" y"+ loc.y);
-    lastItem = xOnY;
+function step(xDir, yDir) {
+    let treesOnSlope = 0;
+    while (loc.y < data.length - 1) {
+        loc.x = loc.x + xDir;
+        loc.y = loc.y + yDir;
+        
+        // Get Y line
+        let yLine = data[loc.y]
 
-    if(xOnY === "#"){
-        console.log("!found a tree at "+ "x"+loc.x +" y"+ loc.y);
-        trees++;
+        // Get X from yLine
+        let xOnY = yLine.charAt(loc.x);
+        console.log("There is a " + xOnY + " at x" + loc.x + " y" + loc.y);
+        lastItem = xOnY;
+
+        if (xOnY === "#") {
+            console.log("!found a tree at " + "x" + loc.x + " y" + loc.y);
+            trees++;
+            treesOnSlope++;
+        }
+
     }
 
+    return treesOnSlope;
+
+
+
+}
+
+function resetLoc() {
+    loc = {
+        x: 0,
+        y: 0
+    }
 }
